93eb828588ca5b61624533fcfe63afd7545addae
narekas <apresyan026@gmail.com>
2023-02-21 14:14:48 +0400

    two init

--- blog/functional_test.py
+++ blog/functional_test.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 class BasicInstallTest(unittest.TestCase):  
 
     def setUp(self):  
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
 
     def tearDown(self):  
          self.browser.quit()
@@ -19,6 +19,17 @@ class BasicInstallTest(unittest.TestCase):
         header = self.browser.find_element(By.TAG_NAME, 'h1')
         self.assertIn('Blog', header.text)
 
+    def test_home_page_blog(self):
+        self.browser.get('http://127.0.0.1:8000')
+        article_list = self.browser.find_element_by_class_name('article-list')
+        self.assertTrue(article_list)
+
+    def test_home_page_articles_look_correct(self):
+        self.browser.get('http://127.0.0.1:8000')
+        article_title = self.browser.find_element_by_class_name('article-title')
+        article_summary = self.browser.find_element_by_class_name('article-summary')
+        self.assertTrue(article_title)
+        self.assertTrue(article_summary)
 
 if __name__ == '__main__':  
     unittest.main()
